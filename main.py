33092b6d7d7480828e65138d76a592dd2d7832ed
V1d1tD <vdixit@kellettschool.com>
2024-11-06 04:43:38 +0000

    ijiou

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# 
+#test
+from random import randint
+
+
+correct = randint(16, 64)
+number = None
+while number != correct:
+
+    print("Guess My Number")
+    number = int(input())
+    if number > correct:
+        print("LOWER!!!")
+    if number < correct:
+        print("HIGHER!!!")
+    if number == correct: 
+        print("YOU WON!!!!!!!!!!!!")
+
+    five_less = correct - 5
+    five_more = correct + 5
+
+    if number >= five_less and number <= five_more:
+        print("ALMOST WITHIN 5!!!")
+
